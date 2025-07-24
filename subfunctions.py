ce51513801bfa220be319e732196fe059c164bd2
Giovanna Alves Barbosa de Oliveira <164058997+GIOV4NN4EC@users.noreply.github.com>
2025-07-23 23:00:04 -0300

    Arquivo com as funções secundárias do sistema

---
+++ subfunctions.py
@@ -0,0 +1,59 @@
+def add_player():
+    pass
+
+def list_players():
+    pass
+
+def edit_player():
+    pass
+
+def remove_player():
+    pass
+
+def schedule_match():
+    pass
+
+def list_matches():
+    pass
+
+def schedule_training():
+    pass
+
+def list_trainings():
+    pass
+
+def add_equipment():
+    pass
+
+def list_equipment():
+    pass
+
+def edit_equipment():
+    pass
+
+def remove_equipment():
+    pass
+
+def register_income():
+    pass
+
+def register_expense():
+    pass
+
+def list_transactions():
+    pass
+
+def generate_financial_report():
+    pass
+
+def create_post():
+    pass
+
+def list_posts():
+    pass
+
+def create_poll():
+    pass
+
+def list_polls():
+    pass
